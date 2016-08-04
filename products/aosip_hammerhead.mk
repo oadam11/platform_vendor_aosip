230ac476378a02dfc0ecc2f133c8c536f7d5eaab
xlxfoxxlx <joshfox87@gmail.com>
2016-08-03 23:04:39 -0400

    Update hammer fingerprint
    Change-Id: I487ac3174264abc273750e8725566c141f305342

--- products/aosip_hammerhead.mk
+++ products/aosip_hammerhead.mk
@@ -1,5 +1,5 @@
 # Inherit AOSP device configuration for shamu.
-$(call inherit-product, device/lge/hammerhead/aosp_hammerhead.mk)
+$(call inherit-product, device/lge/hammerhead/full_hammerhead.mk)
 
 # Inherit common product files.
 $(call inherit-product, vendor/aosip/products/common.mk)
@@ -10,12 +10,11 @@ PRODUCT_NAME := aosip_hammerhead
 PRODUCT_BRAND := google
 PRODUCT_DEVICE := hammerhead
 PRODUCT_MODEL := Nexus 5
-PRODUCT_MANUFACTURER := lge
+PRODUCT_MANUFACTURER := LGE
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=hammerhead \
-    BUILD_FINGERPRINT=google/hammerhead/hammerhead:6.0.1/MOB30D/2704746:user/release-keys \
-    PRIVATE_BUILD_DESC="hammerhead-user 6.0.1 MOB30D 2704746 release-keys" \
-    BUILD_DISPLAY_ID=MOB30D \
-    BUILD_ID=MOB30D
-
+    BUILD_FINGERPRINT=google/hammerhead/hammerhead:6.0.1/MOB30Y/3067468:user/release-keys \
+    PRIVATE_BUILD_DESC="hammerhead-user 6.0.1 MOB30Y 3067468 release-keys" \
+    BUILD_DISPLAY_ID=MOB30Y \
+    BUILD_ID=MOB30Y
