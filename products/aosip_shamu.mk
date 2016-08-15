bf8ed97386479dda0edca31f0d6a846fcce95db6
Alx31 <alex31.couderc@googlemail.com>
2016-08-15 13:10:44 +0200

    update shamu fingerprint and missing targets
    Change-Id: I332dd9cf6194a652cab76930bf8d937b8813709e

--- products/aosip_shamu.mk
+++ products/aosip_shamu.mk
@@ -1,20 +1,28 @@
-# Inherit AOSP device configuration for shamu.
+# aosip configuration
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2560
+TARGET_SCREEN_WIDTH := 1440
+
+# Inherit device configuration
 $(call inherit-product, device/moto/shamu/aosp_shamu.mk)
 
 # Inherit common product files.
 $(call inherit-product, vendor/aosip/products/common.mk)
 
-# Setup device specific product configuration.
+DEVICE_PACKAGE_OVERLAYS += device/moto/shamu/overlay-cm
+
+## Device identifier. This must come after all inclusions
 PRODUCT_NAME := aosip_shamu
-PRODUCT_BRAND := google
 PRODUCT_DEVICE := shamu
+PRODUCT_BRAND := Google
 PRODUCT_MODEL := Nexus 6
 PRODUCT_MANUFACTURER := motorola
+PRODUCT_RESTRICT_VENDOR_FILES := false
 
-# Fingerprint
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=shamu \
-    BUILD_FINGERPRINT=google/shamu/shamu:6.0.1/MOB30M/2862625:user/release-keys \
-    PRIVATE_BUILD_DESC="shamu-user 6.0.1 MOB30M 2862625 release-keys" \
-    BUILD_ID=MOB30M \
-    BUILD_DISPLAY_ID=MOB30M
+    BUILD_FINGERPRINT=google/shamu/shamu:6.0.1/MOB30W/3031100:user/release-keys \
+    PRIVATE_BUILD_DESC="shamu-user 6.0.1 MOB30W 3031100 release-keys" \
+    BUILD_ID=MOB30W \
+    BUILD_DISPLAY_ID=MOB30W
